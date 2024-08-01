68917459ea285563274a96ac3743df7a6a54ca42
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-08-01 10:25:23 +0300

    add manifest

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
 import 'styles/index.scss';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <BrowserRouter basename="/Petlove">
-            <App />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter basename="/Petlove">
+                <App />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
